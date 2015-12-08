a3fe0b9d41fdf6a62418a7ba75f9b25a6d0b1202
Charles Francoise <charles.francoise@gmail.com>
2015-12-08 22:11:10 +0100

    first import

---
+++ js/main.js
@@ -0,0 +1,14 @@
+SC.initialize({
+    client_id : 'd8fed5233037c3f307ebdc1e0da29e4e',
+    redirect_uri : 'http://localhost:8080/callback.html'
+  });
+
+$(document).ready(function() {
+    $('#login-button').click(function(event) {
+        var n = SC.connect().then(function(){
+            return SC.get('/me');
+        }).then(function(me){
+            console.log(me);
+        });
+    });
+});
