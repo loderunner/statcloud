818593ff82d563e0d6693725e58f4dab127b33f9
Charles Francoise <charles.francoise@gmail.com>
2015-12-09 16:45:46 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 var config = {
-    client_id : '7eb81e1057cd47e19c74b50dff24e1bc',
-    redirect_uri : 'http://loderunner.github.io/statcloud/callback.html'
+    client_id : '',
+    redirect_uri : ''
 };
