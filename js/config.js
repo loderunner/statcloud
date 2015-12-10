add9a87f80b173f9983a3ae750ce129c919cb2c1
Charles Francoise <charles.francoise@gmail.com>
2015-12-10 21:58:00 +0100

    add credentials

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 var config = {
-    client_id : '',
-    redirect_uri : ''
+    client_id : '7eb81e1057cd47e19c74b50dff24e1bc',
+    redirect_uri : 'http://loderunner.github.io/statcloud/callback.html'
 };
